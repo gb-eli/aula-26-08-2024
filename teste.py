e89df78b8a028841e91bac6acbd874033a56788a
Gabriel Elienai <162762339+gb-eli@users.noreply.github.com>
2024-08-26 14:14:12 -0300

    teste

---
+++ teste.py
@@ -0,0 +1,26 @@
+print("Olá")
+
+name = input("Qual o seu nome?")
+
+print("Bem vindo" + name)
+
+print("Escolha uma das opções abaixo:")
+print("1 - Falar com atendente")
+print("2 - Ligar")
+print("3 - Finalizar pedido")
+
+opcao = int(input("Escolha uma opção"))
+
+    if opcao == 1:
+        print("Aguarde, logo voce ser[a atendido]")
+
+    elif opcao == 2:
+        print("O número é 08007070")
+
+    elif opcao == 3:
+        print("O número é 08007070")
+
+    else:
+        print("Opção incorreta!")
+
+    
